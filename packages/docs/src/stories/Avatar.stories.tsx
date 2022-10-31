a90cecd3f434bbc6f33dfe25e1abfc88f6435fa3
Gabriel <gabriel.fernandesb@gmail.com>
2022-10-30 22:37:33 -0300

    feat: components core

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/gbeserra95.png',
+    alt: 'Gabriel Beserra',
+  },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
